4d9e8c0267659c3fb6f3780f51a84c6e1997f9c3
SokurV <windows607@gmail.com>
2023-02-01 21:07:19 +0600

    Обновление
    Обновил структуру стилей, переименовал классы.
    Добавил запись истории нажатых кнопок и её очистку.
    Добавил функцию воспроизведения звуков согласно истории.
    Но она не работает.

--- src/script/DrumMachine.js
+++ src/script/DrumMachine.js
@@ -4,35 +4,93 @@ export default class DrumMachineApp extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            lastPressedButton: 'click button'.toUpperCase()
+            lastPressedButton: 'click button'.toUpperCase(),
+            history: []
         }
         this.padsRender = this.padsRender.bind(this)
         this.clickCallback = this.clickCallback.bind(this)
         this.keydownCallback = this.keydownCallback.bind(this)
+        this.historyGenerate = this.historyGenerate.bind(this)
+        this.clearHistory = this.clearHistory.bind(this)
+        this.playHistory = this.playHistory.bind(this)
+        this.play = this.play.bind(this)
     }
 
     clickCallback(event){
-        event.target.classList.add('drumPad_clickHover')
+        event.target.classList.add('drum-pad_click')
         const audioID = event.target.textContent
         document.getElementById(audioID).play()
-        setTimeout(()=>event.target.classList.remove('drumPad_clickHover'), 200)
+        setTimeout(()=>event.target.classList.remove('drum-pad_click'), 200)
         this.setState(()=>{
             return {
                 lastPressedButton: event.target.id
             }
         })
+        this.historyGenerate(audioID)
     }
 
     keydownCallback(event){
-        const audio = document.getElementById(event.code.slice(-1))
-        audio.parentNode.classList.add('drumPad_clickHover')    
-        audio.play()
-        setTimeout(()=>audio.parentNode.classList.remove('drumPad_clickHover'), 200)
-        this.setState(()=>{
+        const padName = event.code.slice(-1)
+        if(['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'].indexOf(padName) >= 0){
+            console.log(padName)
+            const audio = document.getElementById(padName)
+            audio.parentNode.classList.add('drum-pad_click')    
+            audio.play()
+            setTimeout(()=>audio.parentNode.classList.remove('drum-pad_click'), 200)
+            this.setState(()=>{
+                return {
+                    lastPressedButton: audio.parentNode.id
+                }
+            })
+            this.historyGenerate(padName) 
+        }
+    }
+
+    historyGenerate(nameButton){
+        this.setState(function(prevState){
             return {
-                lastPressedButton: audio.parentNode.id
+                history: prevState['history'].concat([nameButton])
             }
-        }) 
+        })
+        let historyBox = document.querySelector('.history-box')
+        let boxItem = document.createElement('div')
+        boxItem.textContent = nameButton
+        boxItem.classList.add('history-box__item', 'history-box__item_style')
+        historyBox.append(boxItem)
+    }
+
+    async playHistory(){
+        const playList = this.state.history
+        if(playList.length > 0){
+            for(let i = 0; i < playList.length; i++){
+                let audioTrack = document.getElementById(playList[i])
+                await this.play(audioTrack)
+            }
+        }
+    }
+
+    play(audio){
+        audio.play()
+        return new Promise((reject, resolve) => {
+            audio.addEventListener('ended', () => {
+                console.log('ended')
+                resolve()
+            })
+    
+            audio.addEventListener('error', () => {
+                reject('Ошибка')
+            })
+        })
+    }
+
+    clearHistory(){
+        this.setState(function(){
+            return {
+                lastPressedButton: 'click button'.toUpperCase(),
+                history: []
+            }
+        })
+        document.querySelector('.history-box').innerHTML = ''
     }
 
     padsRender(i){
@@ -56,7 +114,7 @@ export default class DrumMachineApp extends React.Component{
                     id={padsID[i]}
                     url={soundsURL[i]}
                     name={padsName[i]}
-                    class='drumPad drumPad_theme' 
+                    class='drum-pad drum-pad_style' 
                     key={`uniqKey_${i}`}
                     onClickFunc={this.clickCallback}
                 />
@@ -67,13 +125,34 @@ export default class DrumMachineApp extends React.Component{
 
     render(){
         return (
-            <div id="drum-machine" className="appContainer appContainer_theme" tabIndex={0} onKeyDown={this.keydownCallback}>
-                <div id='display' className='display display_theme'>
-                    <p>{this.state.lastPressedButton}</p>
+            <div 
+                id="drum-machine" 
+                className="container container_style" 
+                tabIndex={0} 
+                onKeyDown={this.keydownCallback}
+            >
+                <div className="drum-machine">
+                    <div id='display' className='display display_style'>
+                        <p>{this.state.lastPressedButton}</p>
+                    </div>
+                    <div className='drum-pads-container'>
+                        {this.padsRender(8)}
+                    </div>
                 </div>
-                <div className='drumPadsContainer'>
-                    {this.padsRender(8)}
-                </div>
+                <div className="history-box"></div>
+                <button 
+                    className="play-button play-button_style" 
+                    onClick={this.playHistory}
+                    disabled
+                >
+                        Кн. недоступна
+                </button>
+                <button 
+                    className="clear-button clear-button_style" 
+                    onClick={this.clearHistory}
+                >
+                        Clear history
+                </button>
             </div>
         )
     }
